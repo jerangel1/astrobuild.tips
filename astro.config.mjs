1af94764cda709c1c51b0d946b1ab895bd7f160f
Jesus Rangel <jerangel1691@gmail.com>
2024-02-06 16:32:17 -0400

    [ADD] update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import vue from '@astrojs/vue';
-import svelte from '@astrojs/svelte';
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte"
+import vue from "@astrojs/vue"
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrobuild.tips',
-  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()],
-  output: "server",
-  adapter: cloudflare()
-});
+  integrations: [tailwind(), mdx(), react(),svelte(),vue(), sitemap()],
+  // output: "server",
+  // adapter: cloudflare({
+  //   imageService: 'cloudflare', // Habilitar la optimización de imágenes de Cloudflare
+  });
+// })
